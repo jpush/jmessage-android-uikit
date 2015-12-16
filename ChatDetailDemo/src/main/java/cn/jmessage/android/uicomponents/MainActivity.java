5d5711d71594dfe488a85b9367c3c2b27fec8f72
caiyg <caiyg@jpush.cn>
2015-12-16 15:23:02 +0800

    1.增加ChatDetailDemo, 2.使用jmessage-1.1.5

---
+++ ChatDetailDemo/src/main/java/cn/jmessage/android/uicomponents/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.jmessage.android.uicomponents;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
